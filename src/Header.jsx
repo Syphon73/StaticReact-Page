0a2fa9d5a38a51e20ed183597c09de8bb6b0b46e
Syphon73 <siddiquishifa78@gmail.com>
2025-03-22 17:31:07 +0530

    updated image in header as it was not loading

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
+import logo from './logo.png';
 export default function Content(){
     return(
         <header className="nav">
-            <img className="image" src='./src/assets/react.svg' width="40px" alt="react logo"></img>
+            <img className="image" src={logo} width="40px" alt="react logo"></img>
             <h2 className="title">ReactFacts</h2>
         </header>
     )
